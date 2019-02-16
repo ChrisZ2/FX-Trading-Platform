64a8e431e250f048b2ca90411033e0f1ada696ea
Chris Zhang <zrzllyz@outlook.com>
2019-02-16 15:44:43 -0500

    Init Commit

---
+++ src/main/java/com/chris/projects/FXTradingPlatform/FxTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.chris.projects.FXTradingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxTradingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxTradingPlatformApplication.class, args);
+	}
+
+}
